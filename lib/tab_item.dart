06c78618e308fcc76405a28eacecde8fb194f257
Benyamin Beyzaie <benyamin.beyzaie@hezardastan.net>
2022-10-30 18:14:54 +0330

    add `circleStrokeColor` field

--- lib/tab_item.dart
+++ lib/tab_item.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+
 class TabItem {
   IconData icon;
   String title;
   Color circleColor;
+  Color? circleStrokeColor;
   TextStyle labelStyle;
 
-  TabItem(this.icon, this.title, this.circleColor, {this.labelStyle = const TextStyle(fontWeight: FontWeight.bold)});
+  TabItem(
+    this.icon,
+    this.title,
+    this.circleColor, {
+    this.circleStrokeColor,
+    this.labelStyle = const TextStyle(fontWeight: FontWeight.bold),
+  });
 }
